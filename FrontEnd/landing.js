078216f18e2eb0ff6df5dbf7bdc796c9f46dd513
yonilip <johnnyhalen@gmail.com>
2016-05-19 22:28:04 +0300

    dynamic div first try

--- FrontEnd/landing.js
+++ FrontEnd/landing.js
@@ -6,7 +6,33 @@
 function insertResults(resultsArray) {
     var index;
     for (index = 0; index < resultsArray.length; ++index) {
-        $('<div>')
+        $(' <div id="resultTemplate">' +
+                '<div class="row container">' +
+                    '<img src="'+ resultsArray[index].image+'" class="img-responsive" alt="Responsive image">' +
+                '</div>' +
+                '<div class="row">' +
+                    '<div class="col-md-3 panel panel-default">' +
+                '<div class="panel-body">' +
+                    resultsArray[index].price +
+                    '</div>' +
+                '</div>' +
+                '<div class="col-md-6 panel panel-default">' +
+                    '<div class="panel-body">' +
+                        resultsArray[index].grade +
+                    '</div>' +
+                '</div>' +
+                '<div class="col-md-3 panel panel-default">' +
+                    '<div class="panel-body">' +
+            ' <span class="badge">'+resultsArray[index].reviews+'</span>' +
+                     '<p>Reviews</p>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+            '<div class="row">' +
+
+            '</div>' +
+            '</div>'
+        ).append(); //TODO check where to append to
     }
 }
 
@@ -15,7 +41,7 @@ function insertResults(resultsArray) {
 
 $(document).ready( function() {
 
-    $("#searchButton").click(function(){
+    $("#searchButton").click(fdatetimepickerunction(){
         var data ={}; //TODO can enter default values here
         data.dest = $("#dest").val();
         data.date = $("#datetimepicker4").val(); //TODO make null
@@ -31,6 +57,7 @@ $(document).ready( function() {
                 console.log("HTTP Request Succeeded: " + jqXHR.status);
                 console.log(data);
                 $("#firstPageAbout").hide(); //TODO here we should switch to 2nd page
+                insertResults(data);
             })
             .fail(function(jqXHR, textStatus, errorThrown) {
                 console

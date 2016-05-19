de0ccb3393bdbf31997494968c87126ef64debff
yonilip <johnnyhalen@gmail.com>
2016-05-19 20:43:40 +0300

    eden look at the js push

--- FrontEnd/landing.js
+++ FrontEnd/landing.js
@@ -3,34 +3,43 @@
  */
 
 
+function insertResults(resultsArray) {
+    var index;
+    for (index = 0; index < resultsArray.length; ++index) {
+        $('<div>')
+    }
+}
+
+
+
+
 $(document).ready( function() {
 
-    $('#datetimepicker4').datetimepicker();
-
- $("#searchButton").click(function(){
-     var data ={'dest':""};
-     data.dest = $("#dest").val();
+    $("#searchButton").click(function(){
+        var data ={}; //TODO can enter default values here
+        data.dest = $("#dest").val();
+        data.date = $("#datetimepicker4").val(); //TODO make null
+        data.interetst = $("#intrests").val(); //TODO make sure this will bring all the li as object (?)
         console.log(data);
-     jQuery.ajax({
-             url: "http://getlocal1.rapidapi.io/search",
-             type: "POST",
-             contentType: "application/x-www-form-urlencoded",
-             data: data
-         })
-         .done(function(data, textStatus, jqXHR) {
-             console.log("HTTP Request Succeeded: " + jqXHR.status);
-             console.log(data);
-             $("#firstPageAbout").hide();
-         })
-         .fail(function(jqXHR, textStatus, errorThrown) {
-             console
-.log("HTTP Request Failed");
-             $("#firstPageAbout").hide();
-         })
-         .always(function() {
-             /* ... */
-         });
+        jQuery.ajax({
+                url: "http://getlocal1.rapidapi.io/search",
+                type: "POST",
+                contentType: "application/x-www-form-urlencoded",
+                data: data
+            })
+            .done(function(data, textStatus, jqXHR) {
+                console.log("HTTP Request Succeeded: " + jqXHR.status);
+                console.log(data);
+                $("#firstPageAbout").hide(); //TODO here we should switch to 2nd page
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                console
+                    .log("HTTP Request Failed");
+            })
+            .always(function() { //TODO probably del this
+                /* ... */
+            });
 
 
- });
+    });
 });
